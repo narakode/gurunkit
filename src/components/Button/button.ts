d70ce4a779ce2b833371da396d97e0454fbe201b
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-05-13 12:51:13 +0700

    feat: base button size

--- src/components/Button/button.ts
+++ src/components/Button/button.ts
@@ -1,27 +1,49 @@
 import { h, type SetupContext } from 'vue';
 
-type ButtonColor = 'light' | 'primary';
+type ButtonColor = 'light' | 'primary' | 'error' | 'warning' | 'secondary';
+type ButtonSize = 'sm' | 'md' | 'lg';
 type ButtonProps = {
   classList?: {
     base?: string;
     colors?: {
       light?: string;
       primary?: string;
+      error?: string;
+      warning?: string;
+      secondary?: string;
+    };
+    sizes?: {
+      sm?: string;
+      md?: string;
+      lg?: string;
     };
   };
   color?: ButtonColor;
+  size?: ButtonSize;
 };
 
 export function Button(props: ButtonProps, context: SetupContext) {
   const colorClassListOptions: Record<ButtonColor, string> = {
     light: props.classList?.colors?.light ?? '',
     primary: props.classList?.colors?.primary ?? '',
+    error: props.classList?.colors?.error ?? '',
+    warning: props.classList?.colors?.warning ?? '',
+    secondary: props.classList?.colors?.secondary ?? '',
+  };
+  const sizeClassListOptions: Record<ButtonSize, string> = {
+    sm: props.classList?.sizes?.sm ?? '',
+    md: props.classList?.sizes?.md ?? '',
+    lg: props.classList?.sizes?.lg ?? '',
   };
   const colorClassList: string = colorClassListOptions[props.color ?? 'light'];
+  const sizeClassList: string = sizeClassListOptions[props.size ?? 'md'];
 
   return h(
     'button',
-    { ...context.attrs, class: [props.classList?.base, colorClassList] },
+    {
+      ...context.attrs,
+      class: [props.classList?.base, colorClassList, sizeClassList],
+    },
     {
       default: context.slots.default,
     },
