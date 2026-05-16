fae0888ac897a099a969c27bedb50660727101ae
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-05-16 19:46:22 +0700

    feat: base checkbox component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
+<script setup>
+import { Checkbox } from './components/checkbox/checkbox';
+import { ref } from 'vue';
+
+const single = ref(true);
+const multi = ref(['test 2']);
+</script>
+
 <template>
-  <div></div>
+  <div>
+    <Checkbox label="Test" v-model="single" />
+
+    {{ single }}
+
+    <Checkbox label="Test 1" input-value="test 1" v-model="multi" />
+    <Checkbox label="Test 2" input-value="test 2" v-model="multi" />
+    <Checkbox label="Test 3" input-value="test 3" v-model="multi" />
+
+    {{ multi }}
+  </div>
 </template>
