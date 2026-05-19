d1201e0b1f288435b7d000c45ac90d97bde01a1c
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-05-19 13:57:39 +0700

    feat: base checkbox label

--- src/components/checkbox/checkbox.spec.ts
+++ src/components/checkbox/checkbox.spec.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from 'vitest';
 import { mount } from '@vue/test-utils';
 import Checkbox from './checkbox';
 
-describe.only('base', () => {
+describe('base', () => {
   test('renders checkbox', () => {
     const wrapper = mount(Checkbox);
 
@@ -25,5 +25,26 @@ describe.only('base', () => {
     expect(wrapper.find('input[type=checkbox]').attributes('name')).toEqual(
       'test',
     );
+  });
+});
+
+describe('label', () => {
+  test('not renders label', () => {
+    const wrapper = mount(Checkbox);
+
+    expect(wrapper.find('label').exists()).toBe(false);
+  });
+
+  test('renders label when exists', () => {
+    const wrapper = mount(Checkbox, {
+      props: {
+        label: 'Test',
+      },
+    });
+
+    const label = wrapper.find('label');
+
+    expect(label.exists()).toBe(true);
+    expect(label.text()).toBe('Test');
   });
 });
--- src/components/checkbox/checkbox.ts
+++ src/components/checkbox/checkbox.ts
@@ -1,6 +1,17 @@
 import { h, type FunctionalComponent } from 'vue';
 
-const Checkbox: FunctionalComponent = (props, context) =>
-  h('div', [h('input', { type: 'checkbox', ...context.attrs })]);
+type CheckboxProps = {
+  label?: string;
+};
+
+const Checkbox: FunctionalComponent<CheckboxProps> = (props, context) =>
+  h('div', [
+    props.label ? h('label', props.label) : null,
+    h('input', { type: 'checkbox', ...context.attrs }),
+  ]);
+
+Checkbox.props = {
+  label: String,
+};
 
 export default Checkbox;
