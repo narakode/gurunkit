832346167ff9e2876307001f783edfe4938d2e04
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-05-23 06:10:22 +0700

    feat: input select focus outline

--- src/components/input/input.ts
+++ src/components/input/input.ts
@@ -9,27 +9,27 @@ export const classList: {
   colors: Record<Color, HasFileClassList>;
   sizes: Record<Size, HasFileClassList>;
 } = {
-  base: 'border focus:outline-0',
+  base: 'border focus:outline-1',
   file: 'pl-0 file:bg-gray-100 file:h-full file:text-gray-900 file:border-r dark:file:bg-gray-700 dark:file:text-white',
   colors: {
     primary: {
-      base: 'bg-white text-blue-700 placeholder-blue-500 border-blue-300 focus:border-blue-600 dark:bg-transparent dark:border-blue-600 dark:focus:border-blue-400 dark:placeholder-blue-600 dark:text-blue-300',
+      base: 'bg-white text-blue-700 placeholder-blue-500 border-blue-300 focus:border-blue-600 focus:outline-blue-600 dark:bg-transparent dark:border-blue-600 dark:focus:border-blue-400 dark:focus:outline-blue-400 dark:placeholder-blue-600 dark:text-blue-300',
       file: 'file:border-blue-300 focus:file:border-blue-600 dark:file:border-blue-600 dark:focus:file:border-blue-400',
     },
     success: {
-      base: 'bg-white text-green-700 placeholder-green-500 border-green-300 focus:border-green-600 dark:bg-transparent dark:border-green-600 dark:focus:border-green-400 dark:placeholder-green-600 dark:text-green-300',
+      base: 'bg-white text-green-700 placeholder-green-500 border-green-300 focus:border-green-600 focus:outline-green-600 dark:bg-transparent dark:border-green-600 dark:focus:border-green-400 dark:focus:outline-green-400 dark:placeholder-green-600 dark:text-green-300',
       file: 'file:border-green-300 focus:file:border-green-600 dark:file:border-green-600 dark:focus:file:border-green-400',
     },
     error: {
-      base: 'bg-white text-red-700 placeholder-red-500 border-red-300 focus:border-red-600 dark:bg-transparent dark:border-red-600 dark:focus:border-red-400 dark:placeholder-red-600 dark:text-red-300',
+      base: 'bg-white text-red-700 placeholder-red-500 border-red-300 focus:border-red-600 focus:outline-red-600 dark:bg-transparent dark:border-red-600 dark:focus:border-red-400 dark:focus:outline-red-400 dark:placeholder-red-600 dark:text-red-300',
       file: 'file:border-red-300 focus:file:border-red-600 dark:file:border-red-600 dark:focus:file:border-red-400',
     },
     warning: {
-      base: 'bg-white text-yellow-700 placeholder-yellow-500 border-yellow-300 focus:border-yellow-600 dark:bg-transparent dark:border-yellow-600 dark:focus:border-yellow-400 dark:placeholder-yellow-600 dark:text-yellow-300',
+      base: 'bg-white text-yellow-700 placeholder-yellow-500 border-yellow-300 focus:border-yellow-600 focus:outline-yellow-600 dark:bg-transparent dark:border-yellow-600 dark:focus:border-yellow-400 dark:focus:outline-yellow-400 dark:placeholder-yellow-600 dark:text-yellow-300',
       file: 'file:border-yellow-300 focus:file:border-yellow-600 dark:file:border-yellow-600 dark:focus:file:border-yellow-400',
     },
     light: {
-      base: 'bg-white text-gray-700 placeholder-gray-400 border-gray-300 focus:border-blue-600 dark:bg-transparent dark:border-gray-600 dark:focus:border-blue-400 dark:placeholder-gray-500 dark:text-gray-300',
+      base: 'bg-white text-gray-700 placeholder-gray-400 border-gray-300 focus:border-blue-600 focus:outline-blue-600 dark:bg-transparent dark:border-gray-600 dark:focus:border-blue-400 dark:focus:outline-blue-400 dark:placeholder-gray-500 dark:text-gray-300',
       file: 'file:border-gray-300 focus:file:border-blue-600 dark:file:border-gray-600 dark:focus:file:border-blue-400',
     },
   },
--- src/components/select/select.ts
+++ src/components/select/select.ts
@@ -8,20 +8,20 @@ export const classList: {
   wrapperSizes: Record<Size, string>;
   sizes: Record<Size, string>;
 } = {
-  base: 'appearance-none border focus:outline-0',
+  base: 'appearance-none border focus:outline-1',
   wrapper:
     "w-fit relative after:block after:content-[''] after:border-l-2 after:border-b-2 after:border-gray-500 after:-rotate-45 has-focus:after:rotate-135 after:absolute",
   colors: {
     primary:
-      'bg-white text-blue-700 placeholder-blue-500 border-blue-300 focus:border-blue-600 dark:bg-transparent dark:border-blue-600 dark:focus:border-blue-400 dark:placeholder-blue-600 dark:text-blue-300',
+      'bg-white text-blue-700 placeholder-blue-500 border-blue-300 focus:border-blue-600 focus:outline-blue-600 dark:bg-transparent dark:border-blue-600 dark:focus:border-blue-400 dark:focus:outline-blue-400 dark:placeholder-blue-600 dark:text-blue-300',
     success:
-      'bg-white text-green-700 placeholder-green-500 border-green-300 focus:border-green-600 dark:bg-transparent dark:border-green-600 dark:focus:border-green-400 dark:placeholder-green-600 dark:text-green-300',
+      'bg-white text-green-700 placeholder-green-500 border-green-300 focus:border-green-600 focus:outline-green-600 dark:bg-transparent dark:border-green-600 dark:focus:border-green-400 dark:focus:outline-green-400 dark:placeholder-green-600 dark:text-green-300',
     error:
-      'bg-white text-red-700 placeholder-red-500 border-red-300 focus:border-red-600 dark:bg-transparent dark:border-red-600 dark:focus:border-red-400 dark:placeholder-red-600 dark:text-red-300',
+      'bg-white text-red-700 placeholder-red-500 border-red-300 focus:border-red-600 focus:outline-red-600 dark:bg-transparent dark:border-red-600 dark:focus:border-red-400 dark:focus:outline-red-400 dark:placeholder-red-600 dark:text-red-300',
     warning:
-      'bg-white text-yellow-700 placeholder-yellow-500 border-yellow-300 focus:border-yellow-600 dark:bg-transparent dark:border-yellow-600 dark:focus:border-yellow-400 dark:placeholder-yellow-600 dark:text-yellow-300',
+      'bg-white text-yellow-700 placeholder-yellow-500 border-yellow-300 focus:border-yellow-600 focus:outline-yellow-600 dark:bg-transparent dark:border-yellow-600 dark:focus:border-yellow-400 dark:focus:outline-yellow-400 dark:placeholder-yellow-600 dark:text-yellow-300',
     light:
-      'bg-white text-gray-700 placeholder-gray-400 border-gray-300 focus:border-blue-600 dark:bg-transparent dark:border-gray-600 dark:focus:border-blue-400 dark:placeholder-gray-500 dark:text-gray-300',
+      'bg-white text-gray-700 placeholder-gray-400 border-gray-300 focus:border-blue-600 focus:outline-blue-600 dark:bg-transparent dark:border-gray-600 dark:focus:border-blue-400 dark:focus:outline-blue-400 dark:placeholder-gray-500 dark:text-gray-300',
   },
   wrapperSizes: {
     sm: 'after:size-1.75 after:top-2.5 after:right-2.5 has-focus:after:top-3.5',
