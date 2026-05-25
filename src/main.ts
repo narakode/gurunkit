d562174d98fb9e1560ad8051bf0b69fda7ff358c
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-05-25 13:49:30 +0700

    update xport

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,19 @@ import Input from './components/input/input';
 import Checkbox from './components/checkbox/checkbox';
 import Radio from './components/radio/radio';
 import Select from './components/select/select';
+import FormItem from './components/form-item/form-item';
+import Modal from './components/modal/modal';
+import Table from './components/table/table';
+import Widget from './components/widget/widget';
 
-export { Button, Input, Checkbox, Radio, Select };
+export {
+  Button,
+  Input,
+  Checkbox,
+  Radio,
+  Select,
+  FormItem,
+  Modal,
+  Table,
+  Widget,
+};
