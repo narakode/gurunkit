d2a161a5562bfa58d7a738db533acc40b7314637
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-08-29 13:52:03 +0700

    feat: alert dark mode

--- src/components/alert/alert.ts
+++ src/components/alert/alert.ts
@@ -6,13 +6,18 @@ export const classList: {
   base: string;
   colors: Record<Color, string>;
 } = {
-  base: 'p-4 rounded-md',
+  base: 'px-4 py-3 rounded-md border flex items-start gap-2',
   colors: {
-    error: 'bg-red-100',
-    light: 'bg-gray-100',
-    primary: 'bg-blue-100',
-    success: 'bg-green-100',
-    warning: 'bg-warning-100',
+    error:
+      'bg-red-100 text-red-700 border-red-300 dark:bg-red-900/50 dark:text-red-300 dark:border-red-900',
+    light:
+      'bg-gray-100 text-gray-700 border-gray-200 dark:bg-gray-800 dark:text-gray-200 dark:border-gray-700',
+    primary:
+      'bg-blue-100 text-blue-700 border-blue-300 dark:bg-blue-900/50 dark:text-blue-300 dark:border-blue-900',
+    success:
+      'bg-green-100 text-green-700 border-green-300 dark:bg-green-900/50 dark:text-green-300 dark:border-green-900',
+    warning:
+      'bg-yellow-100 text-yellow-700 border-yellow-300 dark:bg-yellow-900/50 dark:text-yellow-300 dark:border-yellow-900',
   },
 };
 
@@ -27,13 +32,19 @@ const Alert: FunctionalComponent<
     'div',
     { class: [classList.base, classList.colors[props.color ?? 'light']] },
     [
-      ctx.slots.icon?.(),
+      ctx.slots.icon
+        ? h('div', { class: 'mt-0.75 shrink-0' }, ctx.slots.icon())
+        : null,
       ctx.slots.default?.(),
       props.closable
         ? h(
             'button',
-            { 'aria-label': 'Close Alert', onClick: () => ctx.emit('close') },
-            () => h(X),
+            {
+              class: 'cursor-pointer ml-auto mt-1 shrink-0',
+              'aria-label': 'Close Alert',
+              onClick: () => ctx.emit('close'),
+            },
+            h(X, { class: 'size-4' }),
           )
         : null,
     ],
