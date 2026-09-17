a92ff7460111ccefa59e7b9027dc8455a55e7d16
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-09-17 13:07:43 +0700

    fix: button outline background color

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -37,7 +37,8 @@ export const classList: {
     },
     light: {
       solid: 'bg-white text-gray-700 hover:bg-gray-100',
-      outline: 'border-gray-300 text-gray-900 hover:bg-gray-100',
+      outline:
+        'border-gray-300 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:text-gray-900',
     },
   },
   sizes: {
